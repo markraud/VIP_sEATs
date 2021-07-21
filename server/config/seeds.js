385e8d2519fe4ab18488436fff696124dd131f23
Jovan <jovan.m.nixon@gmail.com>
2021-07-20 20:09:46 -0600

    iteams

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -6,10 +6,10 @@ db.once('open', async () => {
 
   const categories = await Category.insertMany([
     { name: 'Food' },
-    { name: 'Household Supplies' },
-    { name: 'Electronics' },
-    { name: 'Books' },
-    { name: 'Toys' }
+    { name: 'Drinks (non-alcoholic)' },
+    { name: 'Beer' },
+    { name: 'Sweets' },
+    { name: 'Swag' }
   ]);
 
   console.log('categories seeded');
@@ -18,110 +18,164 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Hot-Dog',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
       image: 'cookie-tin.jpg',
       category: categories[0]._id,
-      price: 2.99,
+      price: 6.50,
       quantity: 500
     },
     {
-      name: 'Canned Coffee',
+      name: 'Nachos',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
       image: 'canned-coffee.jpg',
       category: categories[0]._id,
-      price: 1.99,
+      price: 7.99,
       quantity: 500
     },
     {
-      name: 'Toilet Paper',
+      name: 'Pizza',
+      description:
+        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+      image: 'canned-coffee.jpg',
+      category: categories[0]._id,
+      price: 6.99,
+      quantity: 500
+    },
+    {
+      name: 'Burger',
+      description:
+        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+      image: 'canned-coffee.jpg',
+      category: categories[0]._id,
+      price: 5.50,
+      quantity: 500
+    },
+    {
+      name: 'Fries',
+      description:
+        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
+      image: 'canned-coffee.jpg',
+      category: categories[0]._id,
+      price: 3.99,
+      quantity: 500
+    },
+    {
+      name: 'Coke',
       category: categories[1]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
       image: 'toilet-paper.jpg',
-      price: 7.99,
+      price: 4.50,
       quantity: 20
     },
     {
-      name: 'Handmade Soap',
+      name: 'Diet Coke',
       category: categories[1]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
       image: 'soap.jpg',
-      price: 3.99,
+      price: 4.50,
       quantity: 50
     },
     {
-      name: 'Set of Wooden Spoons',
+      name: 'Sprite',
       category: categories[1]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
       image: 'wooden-spoons.jpg',
-      price: 14.99,
+      price: 4.50,
       quantity: 100
     },
     {
-      name: 'Camera',
+    name: 'Bottle Water',
+    category: categories[1]._id,
+    description:
+      'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+    image: 'wooden-spoons.jpg',
+    price: 4.00,
+    quantity: 100
+  },
+    {
+      name: 'Coors Light',
       category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
       image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+      price: 7.25,
+      quantity: 100
     },
     {
-      name: 'Tablet',
+      name: 'Bud Light',
       category: categories[2]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
+      price: 7.25,
+      quantity: 100
     },
     {
-      name: 'Tales at Bedtime',
+      name: 'Popcorn',
       category: categories[3]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
       image: 'bedtime-book.jpg',
-      price: 9.99,
+      price: 8.00,
       quantity: 100
     },
     {
-      name: 'Spinning Top',
+      name: 'Cotton Candy',
+      category: categories[3]._id,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
+      image: 'bedtime-book.jpg',
+      price: 4.50,
+      quantity: 100
+    },
+    {
+      name: 'Ice Cream',
+      category: categories[3]._id,
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
+      image: 'bedtime-book.jpg',
+      price: 4.50,
+      quantity: 100
+    },
+    {
+      name: 'Baseball Hat',
       category: categories[4]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
       image: 'spinning-top.jpg',
-      price: 1.99,
+      price: 29.99,
       quantity: 1000
     },
     {
-      name: 'Set of Plastic Horses',
+      name: 'Foam-Finger',
       category: categories[4]._id,
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
       image: 'plastic-horses.jpg',
-      price: 2.99,
+      price: 15.00,
       quantity: 1000
     },
     {
-      name: 'Teddy Bear',
+      name: 'Little-Bat',
       category: categories[4]._id,
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
       image: 'teddy-bear.jpg',
-      price: 7.99,
+      price: 17.99,
       quantity: 100
     },
     {
-      name: 'Alphabet Blocks',
+      name: 'Baseball',
       category: categories[4]._id,
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
       image: 'alphabet-blocks.jpg',
-      price: 9.99,
+      price: 17.99,
       quantity: 600
     }
   ]);
