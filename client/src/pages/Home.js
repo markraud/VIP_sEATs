23d0fa2293dbb38c68114f931830afe57f7be2b2
Janée Artis <janeeartis@gmail.com>
2021-07-25 21:59:54 -0600

    commented out text animation

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,29 @@
 import React from "react";
-
-
+// import { init } from "ityped";
+// import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom"
 
+
+
+
 const Home = () => {
+
+  // const textRef = useRef();
+
+  // useEffect(()=> {
+  //     init(textRef.current, {
+  //         backDelay: 1500,
+  //         backSpeed: 60,
+  //         showCurser: true,
+  //         strings: ["Welcome to VIP sEATs"]
+  //     })
+  //   },[])
+
   return (
     <div className="container">
       
       <Link exact to="/itemsAll">Go to all items</Link>
+      {/* <h3 id="hometext"><span ref={textRef}></span></h3> */}
       
     </div>
   );
